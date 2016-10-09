0560ac6ff0d79e21060440e815fd7bd8a4b69d0d
RCODI <opendigital@purdue.edu>
2016-10-08 22:20:19 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# blackironhack-phase3-11
+# [TravelSmart](http://ironhack-danzheng.herokuapp.com)
+
+_Note: this project is in progress. More updates are on the way!_
+
+## Description
+
+TravelSmart is a web app that helps people make informed travel decisions.
+
+Users input a starting location, a destination, and their mode of travel. The app then performs calculations based on the datasets and displays relevant information about the path of travel. User can make travel decisions based on the information.
+
+## Datasets
+
+- Global Climate Network: [CDO API for GHCN Daily Summaries](http://www.ncdc.noaa.gov/cdo-web/webservices/v2)
+- Wunderground: [10-Day Forecast API](https://www.wunderground.com/weather/api/)
+- Google Maps JavaScript API: [Map and Directions services](https://developers.google.com/maps/documentation/javascript/3.exp/reference)
+
+_more to be determined_
+
+## Requirements
+
+- Node
+- NPM
+
+## Installation
+
+To install packages and start the app, run:
+
+```bash
+npm install # install packages
+node app.js # start app on 'http://localhost:3000'
+```
+
+I run my app using Google Chrome, but it should be compatible with most browsers. The app is not currently mobile friendly.
+
+## Technologies
+
+Google Maps, Node.js, d3.js, c3.js
+
+## Keywords
+
+transportation, travel, safety, health, weather, disease
---
+++ app.js
@@ -0,0 +1,57 @@
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const errorHandler = require('errorhandler');
+const flash = require('express-flash');
+const path = require('path');
+const passport = require('passport');
+const sass = require('node-sass-middleware');
+
+/**
+ * Controllers (route handlers).
+ */
+const homeController = require('./controllers/home');
+
+/**
+ * Create Express server.
+ */
+const app = express();
+
+/**
+ * Express configuration.
+ */
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(compression());
+app.use(sass({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public')
+}));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+
+/**
+ * Primary app routes.
+ */
+app.get('/', homeController.index);
+
+/**
+ * Error Handler.
+ */
+app.use(errorHandler());
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
+
+module.exports = app;
